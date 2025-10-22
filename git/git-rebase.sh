4bf0bed0716d292655e61bde7fa43a5a3c6909c0
Andy Lee <alee@icr-team.com>
2025-10-22 12:14:50 -0400

    Update Git rebase doc

--- git/git-rebase.sh
+++ git/git-rebase.sh
@@ -1,9 +1,20 @@
 ### Git Rebase
-# Assumptions: Feature branch is "fbranch"; assume remote fbranch is already in rebase hell, where it shows a huge number of commits and changes... 
 
+# Assumptions: Remote working branch is already in rebase hell, where it shows a huge number of commits and changes... 
 git checkout <commit-hash> # commit where you actually want to rebase on
-git switch -c fbranch #this branch already existing in remote but re-create it locally anyway
+git switch -c <feature-branch> #this branch already existing in remote but re-create it locally anyway
 git fetch origin
 git rebase main
 git add .; git commit -m "Commit message"
-git push --force-with-lease origin fbranch
+git push --force-with-lease origin <feature-branch>
+
+
+# Assumptions: Remote working branch is not in rebase hell, but many commits behind main
+git fetch origin
+git rebase main
+# VS Code: Source Control > Merge Changes > fix, save > Click + (Stage Changes)
+git rebase --continue
+git add <file>
+# Keep running git rebase --continue until all conflicts resolved
+git add .; git commit -m "Commit message"
+git push --force-with-lease origin <feature-branch>
